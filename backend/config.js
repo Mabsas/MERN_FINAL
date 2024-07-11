89d52e76a55bf4cb2c612b39d75e0bd41db4c746
Mabsas <139257914+Mabsas@users.noreply.github.com>
2024-07-12 01:07:31 +0600

    mongodb and server setup

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://root:root1234root@mern-final.ljka2pv.mongodb.net/mern-collection?retryWrites=true&w=majority&appName=Mern-final'
