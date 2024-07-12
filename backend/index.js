96e936b2b7ff481a3144dba674b6d82986ec280e
Mabsas <139257914+Mabsas@users.noreply.github.com>
2024-07-12 18:24:17 +0600

    Settingupfrontend

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,9 @@ import express, { request, response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
-import booksRoute from './routes/booksRoute.js'
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
+
 
 
 
@@ -11,6 +13,18 @@ const app = express();
 
 // Middleware for parsing request body
 app.use(express.json());
+
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
+app.use(cors());
+// Option 2: Allow Custom Origins
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   })
+// );
 
 app.get('/', (request, response) => {
 
