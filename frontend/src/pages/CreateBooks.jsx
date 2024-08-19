0d12d63ac841fd55d7f9bcd6777607f4e07e5f30
Mabsas <139257914+Mabsas@users.noreply.github.com>
2024-08-20 00:15:25 +0600

    create css

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -3,6 +3,7 @@ import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import styles from '../css/CreateBook.module.css'; // Import the CSS module
 
 const CreateBook = () => {
 
@@ -37,56 +38,58 @@ const CreateBook = () => {
   };
 
   return (
-    <div className='p-4'>
+    <div className={styles.container}>
       <BackButton />
-      <h1 className='text-3xl my-4'>Create Book</h1>
+      <div className={styles.header}>
+        <h1 className={styles.title}>Create Book</h1>
+      </div>
       {loading ? <Spinner /> : ''}
-      <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
-        <div className='my-4'>
-          <label className='text-xl mr-4 text-gray-500'>Title</label>
+      <div className={styles.formContainer}>
+        <div className={styles.formField}>
+          <label className={styles.formLabel}>Title</label>
           <input
             type='text'
             value={title}
             onChange={(e) => setTitle(e.target.value)}
-            className='border-2 border-gray-500 px-4 py-2 w-full'
+            className={styles.input}
           />
         </div>
-        <div className='my-4'>
-          <label className='text-xl mr-4 text-gray-500'>Author</label>
+        <div className={styles.formField}>
+          <label className={styles.formLabel}>Author</label>
           <input
             type='text'
             value={author}
             onChange={(e) => setAuthor(e.target.value)}
-            className='border-2 border-gray-500 px-4 py-2  w-full '
+            className={styles.input}
           />
         </div>
-        <div className='my-4'>
-          <label className='text-xl mr-4 text-gray-500'>Publish Year</label>
+        <div className={styles.formField}>
+          <label className={styles.formLabel}>Publish Year</label>
           <input
             type='number'
             value={publishYear}
             onChange={(e) => setPublishYear(e.target.value)}
-            className='border-2 border-gray-500 px-4 py-2  w-full '
+            className={styles.input}
           />
         </div>
-        <div className='my-4'>
-          <label className='text-xl mr-4 text-gray-500'>Description</label>
+        <div className={styles.formField}>
+          <label className={styles.formLabel}>Description</label>
           <textarea
             value={description}
             onChange={(e) => setDescription(e.target.value)}
-            className='border-2 border-gray-500 px-4 py-2 w-full'
+            className={styles.textarea}
           />
         </div>
-        <div className='my-4'>
-          <label className='text-xl mr-4 text-gray-500'>Picture URL</label>
+        <div className={styles.formField}>
+          <label className={styles.formLabel}>Picture URL</label>
           <input
             type='text'
             value={pictureURL}
             onChange={(e) => setPictureURL(e.target.value)}
-            className='border-2 border-gray-500 px-4 py-2 w-full'
+            className={styles.input}
           />
         </div>
-        <button className='p-2 bg-sky-300 m-8' onClick={handleSaveBook}>
+        <button className={styles.button} onClick={handleSaveBook}>
           Save
         </button>
       </div>
